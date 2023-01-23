a901fd2006a8fdc32ff6be87b7dd69b00e18147c
ari <aryfareq03@gmail.com>
2023-01-23 12:07:44 +0300

    Update suggestion.js

--- suggestion.js
+++ suggestion.js
@@ -3,26 +3,18 @@ const cities = [];
 fetch (endpoint)
 .then(blob => blob.json())
 .then(data => cities.push(...data))
-function matchesFound (wordmatches, cities){
-    return cities.filter(place =>{
-        const regex = new.RegExp(wordmatches, 'gi');
-        return place.cities.match(regex) || place.state.match(regex)
-    })
-};
-function displayem (){
-const matchesArray = matchesFound(this.value, cities);
-console.log(matchesArray);
-const html = matchesArray.map(place =>{
-    return `
-    <li>
-    <span class="name">${place.city},${place.state}</span>
-    <span class="population">${place.population}</span>
-    </li>
-    `;
-}).join('');
-suggestions.innerHTML = html;
+
+function findMatches(wordsToMatch, cities){
+return cities.filter(place => {
+    const regex = new RegExp(wordsToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex)
+});
 }
-const inputs = document.querySelector(.search);
-const inputs = document.querySelector(.suggestions);
-searchInput.addEventListener('change', displayem);
-searchInput.addEventListener('keyup', displayem);
+
+function displayMatches (){
+    console.log(this.value)
+}
+const searchInput = document.querySelectorAll('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('keyup',displayMatches)
